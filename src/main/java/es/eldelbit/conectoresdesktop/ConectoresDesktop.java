e2998e0c335a30c53f0b5dfdcf22bdb8f10c0702
Antonio José García Jiménez <aj@losgarciajimenez.com>
2023-02-09 00:04:47 +0100

    Proyecto Creado
    NetBeans. Java with Maven. Java Application

---
+++ src/main/java/es/eldelbit/conectoresdesktop/ConectoresDesktop.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package es.eldelbit.conectoresdesktop;
+
+/**
+ *
+ * @author virtualbox
+ */
+public class ConectoresDesktop {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
